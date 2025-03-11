38edaa16791ef5f29fbab5c74f9a453168523309
tawkeer1 <towqeerahmad111@gmail.com>
2025-03-11 20:02:59 +0530

    used auth() instead of currentUser()

--- app/api/project/create-project/route.js
+++ app/api/project/create-project/route.js
@@ -1,10 +1,18 @@
-import Project from '@/models/projects.models';
-import { connect } from '@/mongodb/mongoose.js';
-
-import { currentUser } from '@clerk/nextjs/server';
+import Project from "@/models/projects.models";
+import { connect } from "@/mongodb/mongoose.js";
+import { auth } from "@clerk/nextjs";
+import { clerkClient } from "@clerk/nextjs/server";
 export const POST = async (req) => {
-  const user = await currentUser();
   try {
+    const { userId } = auth();
+    console.log("userID form auth :", userId);
+    if (!userId) return new Response("user doesn't exist", { status: 401 });
+
+    const user = clerkClient.users.getUser(userId);
+    const userMongoId = (await user).publicMetadata?.userMongoId;
+    if (!userMongoId)
+      return new Response("userMongoId missing", { status: 400 });
+
     await connect();
     const data = await req.json();
 
@@ -17,23 +25,21 @@ export const POST = async (req) => {
     //     status: 401,
     //   });
     // }
-    console.log("user :", user);
-    if(!user) return new Response("user doesn't exist")
     const newPost = await Project.create({
-      userId: user.publicMetadata?.userMongoId,
+      userId: userMongoId,
       content: data.content,
       title: data.title,
       image: data.image,
       url: data.url,
     });
     await newPost.save();
-    console.log("project at backend",newPost)
+    console.log("project at backend", newPost);
     return new Response(JSON.stringify(newPost), {
       status: 200,
     });
   } catch (error) {
-    console.log('Error creating post:', error);
-    return new Response('Error creating post', {
+    console.log("Error creating post:", error);
+    return new Response("Error creating post", {
       status: 500,
     });
   }
